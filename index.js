7fcd2d0e08f69816674d6d55496ecdc705644c5e
Shyam Chadha <shyamchadha34@gmail.com>
2022-11-10 23:36:31 +0530

    Search Donor Created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-var uniqueValidator = require('mongoose-unique-validator')
+var uniqueValidator = require("mongoose-unique-validator");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
@@ -31,23 +31,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 try {
-  mongoose.connect(
-    "mongodb://localhost:27017/Blood_bank"
-  );
+  mongoose.connect("mongodb://localhost:27017/Blood_bank");
   console.log("MongoDB connected");
 } catch (err) {
   console.log(err);
 }
 
 const userSchema = new mongoose.Schema({
-  username:String,
-  blood_group:String,
-  password:String,
-  phone:String,
-  flatno:String,
-  addline1:String,
-  state:String,
-  pincode:String,
+  username: String,
+  blood_group: String,
+  password: String,
+  phone: String,
+  flatno: String,
+  addline1: String,
+  state: String,
+  pincode: String,
 });
 
 const donorSchema = new mongoose.Schema({
@@ -56,14 +54,17 @@ const donorSchema = new mongoose.Schema({
     required: true,
     ref: "User",
   },
-  disease:String,
-  otherdisease:String,
-  tatoo:String,
-  age:String,
-  drinking:String,
-  smoking:String,
-  donatedbefore:String,
-  vaccinated:String
+  disease: String,
+  otherdisease: {
+    type: String,
+    default: "None",
+  },
+  tatoo: String,
+  age: String,
+  drinking: String,
+  smoking: String,
+  donatedbefore: String,
+  vaccinated: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -78,8 +79,8 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.get("/",function(req,res){
-    res.render("index");
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
 app.get("/register", function (req, res) {
@@ -87,17 +88,38 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  const { username, blood_group, pincode, phone, password, address } = req.body;
-  User.register({ username: username,blood_group:blood_group,pincode:pincode,phone:phone,address:address  }, password, (err, user) => {
-    if (err) {
-      console.log(err);
-      res.redirect("/error");
-    } else {
-      passport.authenticate("local")(req, res, () => {
-        res.redirect("/login");
-      });
+  const {
+    username,
+    blood_group,
+    phone,
+    password,
+    flatno,
+    line1,
+    state,
+    pincode,
+  } = req.body;
+  User.register(
+    {
+      username: lodash.capitalize(username),
+      blood_group: blood_group,
+      phone: phone,
+      flatno: flatno,
+      addline1: line1,
+      state: state.toUpperCase(),
+      pincode: pincode,
+    },
+    password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/error");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/login");
+        });
+      }
     }
-  });
+  );
 });
 
 app.get("/logout", function (req, res) {
@@ -118,7 +140,7 @@ app.post("/login", function (req, res) {
 
   req.login(user, (err) => {
     if (err) {
-        // res.json({ success: false, message: "Username or Password was wrong" });
+      // res.json({ success: false, message: "Username or Password was wrong" });
       console.log(err);
       res.redirect("/");
     } else {
@@ -129,9 +151,9 @@ app.post("/login", function (req, res) {
   });
 });
 
-app.get("/eligibility",function(req,res){
+app.get("/eligibility", function (req, res) {
   // if (req.isAuthenticated()) {
-    res.render("eligibility" , {title:req.user.username});
+  res.render("eligibility", { title: req.user.username });
   // }
 });
 
@@ -139,13 +161,13 @@ app.post("/eligibility", function (req, res) {
   const donor = new Donor({
     user: req.user._id,
     disease: req.body.DISEASES,
-    otherdisease:req.body.OTHER_DISEASE.toUpperCase(),
+    otherdisease: req.body.OTHER_DISEASE.toUpperCase(),
     tatoo: req.body.TATOO,
     age: req.body.AGE_GROUP,
     drinking: req.body.DRINKING,
     smoking: req.body.SMOKING,
     donatedbefore: req.body.DONATED_BEFORE,
-    vaccinated: req.body.VACCINATED
+    vaccinated: req.body.VACCINATED,
   });
 
   donor.save(function (err) {
@@ -158,20 +180,33 @@ app.post("/eligibility", function (req, res) {
 });
 
 app.get("/journey", function (req, res) {
-    if (req.isAuthenticated()) {
-      res.render("journey" , {title:req.user.username});
-    } else {
-      res.redirect("/login");
-    }
-  });
+  if (req.isAuthenticated()) {
+    res.render("journey", { title: req.user.username });
+  } else {
+    res.redirect("/login");
+  }
+});
 
-
-app.get("/addressbook" , function(req,res){
+app.get("/addressbook", function (req, res) {
   res.render("Addressbook");
 });
 
-app.get("/error",function (req,res) {
-  res.sendFile(path.join(__dirname + '/public/error.html'));
+app.get("/error", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/error.html"));
+});
+
+app.get("/donors", function (req, res) {
+  if (req.isAuthenticated()) {
+    Donor.find({ disease: "NONE" }, function (err, donor) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("donors", { arr: donor, title: req.user.username });
+      }
+    }).populate("user");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 let port = process.env.PORT;
